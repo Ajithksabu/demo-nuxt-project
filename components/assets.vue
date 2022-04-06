0af9b75f8be7c0cbdde62f8873e833fcc599ec18
Ajith <ajithks797@gmail.com>
2022-04-06 10:13:55 +0530

    tab view + general details and assests tab contents + pagination

---
+++ components/assets.vue
@@ -0,0 +1,57 @@
+<template>
+  <tbody class="divide-y divide-gray-200 bg-white">
+    <tr>
+      <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+        <div class="font-medium text-gray-900">{{ assetType }}</div>
+      </td>
+      <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+        <div class="text-gray-900">{{ assetCode }}</div>
+      </td>
+      <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+        {{ issueDate }}
+      </td>
+      <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+        <span
+          class="
+            inline-flex
+            rounded-full
+            bg-green-100
+            px-2
+            text-xs
+            font-semibold
+            leading-5
+            text-green-800
+          "
+          >{{ status }}</span
+        >
+      </td>
+      <td
+        class="
+          relative
+          whitespace-nowrap
+          py-4
+          pl-3
+          pr-4
+          text-right text-sm
+          font-medium
+          sm:pr-6
+        "
+      >
+        <a href="#" class="text-gray-600 hover:text-indigo-900"
+          >Edit<span class="sr-only">, {{ assetCode }}</span></a
+        >
+      </td>
+    </tr>
+  </tbody>
+</template>
+<script>
+export default {
+  name: "Assets",
+  props: {
+    assetType: String,
+    assetCode: String,
+    issueDate: String,
+    status: String,
+  },
+};
+</script>
