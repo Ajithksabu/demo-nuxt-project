f21c175ad87742c361ce82a29c136088a01b1f16
Ajith <ajithks797@gmail.com>
2022-04-07 15:54:26 +0530

    accordion-implementation

--- components/employeeDetails/contactDetails.vue
+++ components/employeeDetails/contactDetails.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="grow items-center justify-center px-5">
     <form action="#" method="POST">
-      <div class="shadow sm:rounded-md sm:overflow-hidden">
+      <div>
         <div class="bg-white py-6 px-4 space-y-6 sm:p-6">
-          <h3 class="text-lg leading-6 font-medium text-gray-900">
+          <!-- <h3 class="text-lg leading-6 font-medium text-gray-900">
             Contact Details
-          </h3>
+          </h3> -->
 
           <div class="grid grid-cols-4 gap-4">
             <div>
@@ -116,6 +116,56 @@
                 "
               />
             </div>
+          </div>
+        </div>
+        <div class="pt-5 mb-4">
+          <div class="flex justify-end">
+            <button
+              type="button"
+              class="
+                bg-white
+                py-2
+                px-4
+                border border-gray-300
+                rounded-md
+                shadow-sm
+                text-sm
+                font-medium
+                text-gray-700
+                hover:bg-gray-50
+                focus:outline-none
+                focus:ring-2
+                focus:ring-offset-2
+                focus:ring-indigo-500
+              "
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              class="
+                ml-3
+                mr-5
+                inline-flex
+                justify-center
+                py-2
+                px-4
+                border border-transparent
+                shadow-sm
+                text-sm
+                font-medium
+                rounded-md
+                text-white
+                bg-indigo-600
+                hover:bg-indigo-700
+                focus:outline-none
+                focus:ring-2
+                focus:ring-offset-2
+                focus:ring-indigo-500
+              "
+            >
+              Save
+            </button>
           </div>
         </div>
       </div>
--- components/employeesGeneralDetails.vue
+++ components/employeesGeneralDetails.vue
@@ -1,61 +1,318 @@
 <template>
-  <div>
-    <GeneralDetails />
-    <ContactDetails />
-    <EmploymentDetails />
-    <EducationalQualification />
-    <PreviousWorkExperience />
-    <CredentialDetails />
-    <References />
-    <div class="pt-5">
-      <div class="flex justify-end">
-        <button
-          type="button"
-          class="
-            bg-white
-            py-2
-            px-4
-            border border-gray-300
-            rounded-md
-            shadow-sm
-            text-sm
-            font-medium
-            text-gray-700
-            hover:bg-gray-50
-            focus:outline-none
-            focus:ring-2
-            focus:ring-offset-2
-            focus:ring-indigo-500
-          "
+  <div id="accordion-collapse" data-accordion="collapse" class="mt-4">
+    <h2 id="accordion-collapse-heading-1">
+      <button
+        type="button"
+        class="
+          flex
+          justify-between
+          items-center
+          p-5
+          w-full
+          font-medium
+          text-left text-gray-500
+          rounded-t-lg
+          border border-b-0 border-gray-200
+          hover:bg-gray-100
+        "
+        @click.prevent="handleAccordion(1)"
+        data-accordion-target="#accordion-collapse-body-1"
+        aria-expanded="true"
+        aria-controls="accordion-collapse-body-1"
+      >
+        <span>General details</span>
+        <svg
+          data-accordion-icon
+          class="w-6 h-6 shrink-0"
+          :class="{ 'rotate-180': accordionNo === 1 }"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
         >
-          Cancel
-        </button>
-        <button
-          type="submit"
-          class="
-            ml-3
-            mr-5
-            inline-flex
-            justify-center
-            py-2
-            px-4
-            border border-transparent
-            shadow-sm
-            text-sm
-            font-medium
-            rounded-md
-            text-white
-            bg-indigo-600
-            hover:bg-indigo-700
-            focus:outline-none
-            focus:ring-2
-            focus:ring-offset-2
-            focus:ring-indigo-500
-          "
+          <path
+            fill-rule="evenodd"
+            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </button>
+    </h2>
+    <div
+      id="accordion-collapse-body-1"
+      class="border border-x-1 border-gray-200"
+      :class="{ hidden: accordionNo !== 1 }"
+      aria-labelledby="accordion-collapse-heading-1"
+    >
+      <GeneralDetails />
+    </div>
+    <h2 id="accordion-collapse-heading-2">
+      <button
+        type="button"
+        class="
+          flex
+          justify-between
+          items-center
+          p-5
+          w-full
+          font-medium
+          text-left text-gray-500
+          border border-b-0 border-gray-200
+          hover:bg-gray-100
+        "
+        @click.prevent="handleAccordion(2)"
+        data-accordion-target="#accordion-collapse-body-2"
+        aria-expanded="false"
+        aria-controls="accordion-collapse-body-2"
+      >
+        <span>Contact details</span>
+        <svg
+          data-accordion-icon
+          class="w-6 h-6 shrink-0"
+          :class="{ 'rotate-180': accordionNo === 2 }"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
         >
-          Save
-        </button>
-      </div>
+          <path
+            fill-rule="evenodd"
+            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </button>
+    </h2>
+    <div
+      id="accordion-collapse-body-1"
+      class="border border-x-1 border-gray-200"
+      :class="{ hidden: accordionNo !== 2 }"
+      aria-labelledby="accordion-collapse-heading-1"
+    >
+      <ContactDetails />
+    </div>
+    <h2 id="accordion-collapse-heading-3">
+      <button
+        type="button"
+        class="
+          flex
+          justify-between
+          items-center
+          p-5
+          w-full
+          font-medium
+          rounded-b-lg
+          text-left text-gray-500
+          border border-gray-200
+          hover:bg-gray-100
+        "
+        @click.prevent="handleAccordion(3)"
+        data-accordion-target="#accordion-collapse-body-3"
+        aria-expanded="false"
+        aria-controls="accordion-collapse-body-3"
+      >
+        <span>Employment details</span>
+        <svg
+          data-accordion-icon
+          class="w-6 h-6 shrink-0"
+          :class="{ 'rotate-180': accordionNo === 3 }"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </button>
+    </h2>
+    <div
+      id="accordion-collapse-body-1"
+      class="border border-x-1 border-gray-200"
+      :class="{ hidden: accordionNo !== 3 }"
+      aria-labelledby="accordion-collapse-heading-1"
+    >
+      <EmploymentDetails />
+    </div>
+    <h2 id="accordion-collapse-heading-3">
+      <button
+        type="button"
+        class="
+          flex
+          justify-between
+          items-center
+          p-5
+          w-full
+          font-medium
+          rounded-b-lg
+          text-left text-gray-500
+          border border-gray-200
+          hover:bg-gray-100
+        "
+        @click.prevent="handleAccordion(4)"
+        data-accordion-target="#accordion-collapse-body-3"
+        aria-expanded="false"
+        aria-controls="accordion-collapse-body-3"
+      >
+        <span>Educational qualifications</span>
+        <svg
+          data-accordion-icon
+          class="w-6 h-6 shrink-0"
+          :class="{ 'rotate-180': accordionNo === 4 }"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </button>
+    </h2>
+    <div
+      id="accordion-collapse-body-1"
+      class="border border-x-1 border-gray-200"
+      :class="{ hidden: accordionNo !== 4 }"
+      aria-labelledby="accordion-collapse-heading-1"
+    >
+      <EducationalQualification />
+    </div>
+    <h2 id="accordion-collapse-heading-3">
+      <button
+        type="button"
+        class="
+          flex
+          justify-between
+          items-center
+          p-5
+          w-full
+          font-medium
+          rounded-b-lg
+          text-left text-gray-500
+          border border-gray-200
+          hover:bg-gray-100
+        "
+        @click.prevent="handleAccordion(5)"
+        data-accordion-target="#accordion-collapse-body-3"
+        aria-expanded="false"
+        aria-controls="accordion-collapse-body-3"
+      >
+        <span>Previous work experience</span>
+        <svg
+          data-accordion-icon
+          class="w-6 h-6 shrink-0"
+          :class="{ 'rotate-180': accordionNo === 5 }"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </button>
+    </h2>
+    <div
+      id="accordion-collapse-body-1"
+      class="border border-x-1 border-gray-200"
+      :class="{ hidden: accordionNo !== 5 }"
+      aria-labelledby="accordion-collapse-heading-1"
+    >
+      <PreviousWorkExperience />
+    </div>
+    <h2 id="accordion-collapse-heading-3">
+      <button
+        type="button"
+        class="
+          flex
+          justify-between
+          items-center
+          p-5
+          w-full
+          font-medium
+          rounded-b-lg
+          text-left text-gray-500
+          border border-gray-200
+          hover:bg-gray-100
+        "
+        @click.prevent="handleAccordion(6)"
+        data-accordion-target="#accordion-collapse-body-3"
+        aria-expanded="false"
+        aria-controls="accordion-collapse-body-3"
+      >
+        <span>Credential details</span>
+        <svg
+          data-accordion-icon
+          class="w-6 h-6 shrink-0"
+          :class="{ 'rotate-180': accordionNo === 6 }"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </button>
+    </h2>
+    <div
+      id="accordion-collapse-body-1"
+      class="border border-x-1 border-gray-200"
+      :class="{ hidden: accordionNo !== 6 }"
+      aria-labelledby="accordion-collapse-heading-1"
+    >
+      <CredentialDetails />
+    </div>
+    <h2 id="accordion-collapse-heading-3">
+      <button
+        type="button"
+        class="
+          flex
+          justify-between
+          items-center
+          p-5
+          w-full
+          font-medium
+          rounded-b-lg
+          text-left text-gray-500
+          border border-gray-200
+          hover:bg-gray-100
+        "
+        @click.prevent="handleAccordion(7)"
+        data-accordion-target="#accordion-collapse-body-3"
+        aria-expanded="false"
+        aria-controls="accordion-collapse-body-3"
+      >
+        <span>References</span>
+        <svg
+          data-accordion-icon
+          class="w-6 h-6 shrink-0"
+          :class="{ 'rotate-180': accordionNo === 7 }"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </button>
+    </h2>
+    <div
+      id="accordion-collapse-body-1"
+      class="border border-x-1 border-gray-200"
+      :class="{ hidden: accordionNo !== 7 }"
+      aria-labelledby="accordion-collapse-heading-1"
+    >
+      <References />
     </div>
   </div>
 </template>
@@ -78,6 +335,20 @@ export default {
     PreviousWorkExperience,
     CredentialDetails,
     References,
+  },
+  data() {
+    return {
+      accordionNo: 1,
+    };
+  },
+  methods: {
+    handleAccordion(accordionValue) {
+      if (this.accordionNo === accordionValue) {
+        this.accordionNo = 0;
+      } else {
+        this.accordionNo = accordionValue;
+      }
+    },
   },
 };
 </script>
